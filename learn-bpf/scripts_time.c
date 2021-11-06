77b25ce3f371412bca6f46500e05a9683e028736
bodhix <zcb973@qq.com>
2021-11-06 22:11:11 +0800

    add some code

--- learn-bpf/scripts_time.c
+++ learn-bpf/scripts_time.c
@@ -5,7 +5,8 @@
  *
  */
 #include <uapi/linux/ptrace.h>
-#include <uapi/linux/bpf.h>
+#include <linux/bpf.h>
+#include <bpf_helpers.h>
 
 #define SEC(NAME) __attribute__((section(NAME), used))
 #define COMMAND_LEN 64
@@ -19,7 +20,7 @@ typedef unsigned long long u64;
 typedef struct _script_stat {
 	u64 start_time_ns;
 	u64 pid;
-	char[COMMAND_LEN] command;
+	char command[COMMAND_LEN];
 } script_stat;
 
 /*
@@ -34,11 +35,21 @@ struct bpf_map_def SEC("maps") script_table = {
 
 
 SEC("kprobe/sys_execve")
-int enter_sys_execve(struct pt_regs *regs)
+int enter_sys_execve(struct pt_regs *regs, const char __user *filename, void *argv, void *envp)
 {
 	// compare executable script name
 	// and record starttime
-	u64 start_time = bpf_ktime_get_ns();
+    char fmt[] = "enter sys execve: %s";
+    script_stat stat = {};
+
+    stat.start_time_ns = bpf_ktime_get_ns();
+    stat.pid = bpf_get_current_pid_tgid();
+    //(void)strncpy(stat.command, filename, sizeof(stat.command));
+    //bpf_get_current_comm(stat.command, sizeof(stat.command));
+    bpf_map_update_elem(&script_table, &stat.pid, &stat, BPF_ANY);
+
+    bpf_trace_printk(fmt, sizeof(fmt), stat.command);
+
 	return 0;
 }
 
@@ -48,10 +59,11 @@ int exit_sys_wait4()
 {
 	// findout specific script by pid
 	// and calculate spendtime
+    char fmt[] = "exit sys wait4";
+    bpf_trace_printk(fmt, sizeof(fmt));
 	u64 end_time = bpf_ktime_get_ns();
 	return 0;
 }
 
 char _license[] SEC("license") = "GPL";
-
 

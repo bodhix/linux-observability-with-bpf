e4353485dbe14a88162dc7e784f10819eaf92657
bodhix <zcb973@qq.com>
2021-11-11 00:22:08 +0800

    add bpf perf event handle in userspace

--- learn-bpf/scripts_time.c
+++ learn-bpf/scripts_time.c
@@ -6,13 +6,15 @@
  */
 #include <linux/ptrace.h>
 #include <linux/bpf.h>
-#include <linux/errno.h>
 #include <bpf_helpers.h>
-#include <string.h>
+#include <linux/string.h>
+
 
 #define SEC(NAME) __attribute__((section(NAME), used))
+
 #define COMMAND_LEN 128
 #define MAP_HASH_MAX_ENTRIES 1024
+#define MAP_ARRAY_MAX_ENTRIES 128
 
 // copy from bpf_helpers.h
 #define PT_REGS_RC(x) ((x)->rax)
@@ -28,15 +30,14 @@ typedef unsigned long long u64;
 typedef unsigned int u32;
 
 /*
- * struct for statistics
+ * for script statistic
  */
 typedef struct _script_stat {
-    u64 time_ns;
     u32 pid;
+    u64 time_ns;
     char parent_comm[COMMAND_LEN];
     char execve_comm[COMMAND_LEN];
 } script_stat;
-
 
 struct execve_args {
     short common_type;
@@ -49,8 +50,20 @@ struct execve_args {
     const char *const *envp;
 };
 
+
+/*
+ * bpf-map to output statistic info to userspace
+ */
+struct bpf_map_def SEC("maps") perf_events = {
+    .type = BPF_MAP_TYPE_PERF_EVENT_ARRAY,
+    .key_size = sizeof(int),
+    .value_size = sizeof(u32),
+    .max_entries = MAP_ARRAY_MAX_ENTRIES
+};
+
 /*
  * bpf-map to store starttime and command for script
+ * for sharing data between kernel bpf prog
  */
 struct bpf_map_def SEC("maps") script_table = {
     .type = BPF_MAP_TYPE_HASH,
@@ -59,32 +72,6 @@ struct bpf_map_def SEC("maps") script_table = {
     .max_entries = MAP_HASH_MAX_ENTRIES
 };
 
-/*
-SEC("tracepoint/syscalls/sys_enter_execve")
-int enter_sys_execve(struct execve_args *args)
-{
-    // compare executable script name
-    // and record starttime
-    char *filename_p = 0;
-
-    script_stat stat = {};
-
-    stat.time_ns = bpf_ktime_get_ns();
-    stat.pid = bpf_get_current_pid_tgid() >> 32;
-    //bpf_probe_read(&filename_p, sizeof(filename_p), args->filename);
-    //bpf_probe_read_str(stat.execve_comm, sizeof(stat.execve_comm), filename_p);
-    bpf_probe_read_str(stat.execve_comm, sizeof(stat.execve_comm), args->filename);
-    bpf_get_current_comm(stat.parent_comm, sizeof(stat.parent_comm));
-
-    
-    char fmt[] = "execve enter: %s, %p\n";
-    bpf_trace_printk(fmt, sizeof(fmt), stat.parent_comm, filename_p);
-
-    char fmt2[] = "execve enter2: %d, %p, %s\n";
-    bpf_trace_printk(fmt2, sizeof(fmt2), stat.pid, stat.execve_comm, stat.execve_comm);
-    return 0;
-}
-*/
 
 //SEC("kprobe/sys_execve")
 SEC("tracepoint/syscalls/sys_enter_execve")
@@ -96,30 +83,51 @@ int enter_sys_execve(struct execve_args *args)
 
     stat.time_ns = bpf_ktime_get_ns();
     stat.pid = bpf_get_current_pid_tgid() >> 32;
-    bpf_probe_read_str(&stat.execve_comm, sizeof(stat.execve_comm), args->filename);
+    bpf_probe_read_str(stat.execve_comm, sizeof(stat.execve_comm), args->filename);
     bpf_get_current_comm(stat.parent_comm, sizeof(stat.parent_comm));
 
-    (void)bpf_map_update_elem(&script_table, &stat.pid, &stat, BPF_NOEXIST);
-
 #ifdef __DEBUG__
-    char fmt[] = "execve enter: %s, %d\n";
+    char fmt[] = "enter execve: %s, pid %d\n";
     bpf_trace_printk(fmt, sizeof(fmt), stat.execve_comm, stat.pid);
 #endif
+
+    (void)bpf_map_update_elem(&script_table, &stat.pid, &stat, BPF_NOEXIST);
     return 0;
 }
 
 
-static int output_script_stat_event(script_stat *stat)
+static int output_stat_event(void *ctx, script_stat *stat)
 {
-    char fmt[] = "execve: %s, pid: %d, time_ns: %lu";
-    bpf_trace_printk(fmt, sizeof(fmt), stat->execve_comm,
+#ifdef __DEBUG__
+    char fmt0[] = "execve parent: %s\n";
+    bpf_trace_printk(fmt0, sizeof(fmt0), stat->parent_comm);
+
+    char fmt1[] = "execve: %s, pid: %d, time_ns: %lu\n";
+    bpf_trace_printk(fmt1, sizeof(fmt1), stat->execve_comm,
                      stat->pid, stat->time_ns);
-    return 0;
+#endif
+
+    script_stat n_stat = {};
+    n_stat.time_ns = stat->time_ns;
+    n_stat.pid = stat->pid;
+    memcpy(n_stat.parent_comm, stat->parent_comm, sizeof(n_stat.parent_comm));
+    memcpy(n_stat.execve_comm, stat->execve_comm, sizeof(n_stat.execve_comm));
+    int ret = bpf_perf_event_output(ctx, &perf_events, 0,
+                                    &n_stat, sizeof(script_stat));
+    if (ret)
+    {
+        char fmt_err[] = "perf event output failed, ret = %d\n";
+        char fmt_err2[] = "execve: %s, pid: %d, time_ns: %lu\n";
+        bpf_trace_printk(fmt_err, sizeof(fmt_err), ret);
+        bpf_trace_printk(fmt_err2, sizeof(fmt_err2), n_stat.execve_comm,
+                         n_stat.pid, n_stat.time_ns);
+    }
+    return ret;
 }
 
 
 SEC("tracepoint/syscalls/sys_enter_exit_group")
-int exit_sys_exit(void *res)
+int exit_sys_exit(void *ctx)
 {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
 
@@ -136,57 +144,13 @@ int exit_sys_exit(void *res)
 
     stat->time_ns = bpf_ktime_get_ns() - stat->time_ns;
 
-    (void)output_script_stat_event(stat);
+    (void)output_stat_event(ctx, stat);
     
     // final delete elem in map
     (void)bpf_map_delete_elem(&script_table, &pid);
-
     return 0;
 }
 
-
-/*
-SEC("kretprobe/sys_wait4")
-int exit_sys_wait4(struct pt_regs *regs)
-{
-    // findout specific script by pid
-    // and calculate spendtime
-    int ret = PT_REGS_RC(regs);
-    u32 pid = 0;
-
-    char fmt[] = "wait4 pid %d return, ret: %d\n";
-    bpf_trace_printk(fmt, sizeof(fmt), bpf_get_current_pid_tgid() >> 32, ret);
-
-    if (ret < 0 && ret != -ECHILD)
-    {
-        return 0;
-    }
-
-    if (pid == -ECHILD)
-    {
-        // get current pid
-        pid = bpf_get_current_pid_tgid() >> 32;
-    }
-    else
-    {
-        pid = ret;
-    }
-    script_stat *stat = bpf_map_lookup_elem(&script_table, &pid);
-    if (!stat)
-    {
-        return 0;
-    }
-
-    stat->time_ns = bpf_ktime_get_ns() - stat->time_ns;
-
-    (void)output_script_stat_event(stat);
-    
-    // final delete elem in map
-    (void)bpf_map_delete_elem(&script_table, &pid);
-
-    return 0;
-}
-*/
 
 char _license[] SEC("license") = "GPL";
 
